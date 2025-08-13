248c780604176f30b9e01044730048bdff07e82c
avarobillard <avamorganr@gmail.com>
2025-08-13 14:11:23 -0700

    storing output from for loops

--- conditionals-ifs-fors.R
+++ conditionals-ifs-fors.R
@@ -121,3 +121,52 @@ for(i in seq_along(tree_height)){
   val <- tree_height[i] + tree_height[i+1]
   print(val)
 }
+
+# For loops with conditional statements
+animal <- c("cat", "dog", "zebra", "dog")
+
+for (i in seq_along(animal)){
+  if (animal[i] == "dog"){
+    print("I love dogs!")
+  } else
+    print("these are other animals!")
+}
+
+# animal species
+species <- c("dog", "elephant", "goat", "dog", "dog", "elephant")
+
+# and their respective ages in human years
+age_human <- c(3, 8, 4, 6, 12, 18)
+
+# convert ages to animal years using the following:
+# 1 human year = 7 in dog years
+# 1 human year = 0.88 in elephant years 
+# 1 human year = 4.7 in goat years
+
+# Allocate a space for our output
+animal_ages <- vector(mode = "numeric", length = length(species))
+
+for(i in seq_along(species)){
+  if (species[i] == "dog"){
+    animal_age <- age_human[i]*7
+  }else if (species[i] == "elephant"){
+    animal_age <- age_human[i]*0.88
+  }else if (species[i] == "goat"){
+    animal_age <- age_human[i]*4.7
+  }
+  animal_ages[i] <- animal_age # populate empty vector
+}
+
+animal_ages
+
+# Another example of storing outputs of a for loop
+tigers <- c(29, 34, 82)
+lions <- c(2, 18, 6)
+
+big_cats <- vector(mode = "numeric", length = length(tigers))
+
+for (i in seq_along(tigers)){
+  big_cats[i] <- tigers[i] + lions[i]
+}
+
+big_cats
